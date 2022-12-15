3b6aaf47f138e228b17561382ed3fc5495d6b69c
aminulislamsohag <38567911+aminulislamsohag@users.noreply.github.com>
2022-12-15 14:15:29 +0600

    loginpage create
    login and logout page create separately.

---
+++ LoginLogout/src/main/java/com/example/demo/LoginLogoutApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginLogoutApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(LoginLogoutApplication.class, args);
+		
+		System.out.println("Hello, World!");
+		
+	}
+
+}
